a1dd001ff0619e6b9d2dbe59204f57c66f94ab42
zwang20 <michael_wzq@icloud.com>
2021-11-19 19:35:38 +1100

    minor changes

---
+++ sign.py
@@ -0,0 +1,45 @@
+"""
+sign.py
+
+signs a public key
+"""
+
+import json
+import os
+
+import rsa
+
+with open("key.pub", "r", encoding="utf-8") as file:
+    public_key = file.read()
+
+user_id = int(input("Enter user id: "), 16)
+
+cert = {
+    "public_key": public_key,
+    "user_id": user_id,
+}
+
+# save cert
+with open('cert.json', 'w', encoding="utf-8") as cert_file:
+    json.dump(cert, cert_file, indent=4)
+
+# load private key
+with open(os.path.join('data', 'keys', 'key'), 'rb') as private_key_file:
+    private_key = rsa.PrivateKey.load_pkcs1(private_key_file.read())
+
+# load public key
+with open(os.path.join('data', 'keys', 'key.pub'), 'rb') as public_key_file:
+    public_key = rsa.PublicKey.load_pkcs1(public_key_file.read())
+
+with open('cert.json', 'r', encoding="utf-8") as cert_file:
+    certificate = cert_file.read().encode('utf-8')
+
+# sign certificate
+signature = rsa.sign(certificate, private_key, 'SHA-512')
+
+# verify sign
+rsa.verify(certificate, signature, public_key)
+
+# save sign
+with open('sign.pem', 'wb') as sign_file:
+    sign_file.write(signature)

9d19d2c49d3ffa2f0664a3912afc4874769d4a5d
zwang20 <michael_wzq@icloud.com>
2021-11-22 14:58:38 +1100

    minor changes

--- sign.py
+++ sign.py
@@ -6,6 +6,7 @@ signs a public key
 
 import json
 import os
+import time
 
 import rsa
 
@@ -14,9 +15,13 @@ with open("key.pub", "r", encoding="utf-8") as file:
 
 user_id = int(input("Enter user id: "), 16)
 
+# get unix timestamp
+timestamp = int(time.time())
+
 cert = {
     "public_key": public_key,
     "user_id": user_id,
+    "timestamp": timestamp,
 }
 
 # save cert
@@ -31,7 +36,8 @@ with open(os.path.join('data', 'keys', 'key'), 'rb') as private_key_file:
 with open(os.path.join('data', 'keys', 'key.pub'), 'rb') as public_key_file:
     public_key = rsa.PublicKey.load_pkcs1(public_key_file.read())
 
-with open('cert.json', 'r', encoding="utf-8") as cert_file:
+# read certificate
+with open(f'{user_id}.json', 'r', encoding="utf-8") as cert_file:
     certificate = cert_file.read().encode('utf-8')
 
 # sign certificate
@@ -41,5 +47,5 @@ signature = rsa.sign(certificate, private_key, 'SHA-512')
 rsa.verify(certificate, signature, public_key)
 
 # save sign
-with open('sign.pem', 'wb') as sign_file:
+with open(f'{user_id}.pem', 'wb') as sign_file:
     sign_file.write(signature)
